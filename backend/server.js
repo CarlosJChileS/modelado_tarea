00cce666275e7f4c752c4acfef39fe7cd3b720d3
CarlosJChileS <carloschilesilva@gmail.com>
2025-05-28 19:04:45 -0500

    aver backend funciona porfa

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
-});
-
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api', require('./routes/auth'));
-app.use('/api/tasks', require('./routes/tasks'));
-
-app.listen(PORT, () => {
-  console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+  res.send('API de Taskmate en ejecución 🚀');
 });
