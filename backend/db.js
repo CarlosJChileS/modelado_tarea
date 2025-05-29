f20da0a19083eb1296426e3ba8c5f3bb6680b471
Michael Intriago <michaelintriago1212@gmail.com>
2025-05-28 22:55:14 -0500

    intento#nosecual funciona porfa

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,14 @@
-const { Pool } = require('pg');
 require('dotenv').config();
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: false, // Necesario para Supabase tanto local como en Azure
+    rejectUnauthorized: false // Usa esto solo si tu servidor de BD lo requiere (Azure, Supabase, Heroku…)
   }
 });
 
